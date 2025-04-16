c01d2899ece0213897132d265acddc9999cd94bb
Bibek Dey <bibek@Bibeks-MacBook-Air.local>
2025-04-16 20:03:07 +0530

    formatted app/clients/page.tsx code and update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  output: 'standalone',
   typescript: {
     ignoreBuildErrors: true,
   },
