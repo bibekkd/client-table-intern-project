1dacd6e824af774c4efa68b6c9a98cc6018d54f6
Bibek Dey <bibek@Bibeks-MacBook-Air.local>
2025-04-16 21:32:49 +0530

    Types added and next.config.ts updated

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  output: 'standalone',
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  
 };
 
 export default nextConfig;
